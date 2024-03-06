602f625a88ed7f01eb466aac104f6ac7c9de43b7
FabricioNascimentop <119469677+FabricioNascimentop@users.noreply.github.com>
2024-03-06 15:12:01 -0300

    site carro v1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from functions import *
 
@@ -38,31 +38,56 @@ class Clientes(db.Model):
 
 
 
+nome_carro_da_semana = 'Acura MDX'
 
 
 @app.route('/')
 def home():
-    nome_carro_da_semana = 'Hyundai Tucson'
     #esta data_preco ajusta a data e os valores para o padrão brasileiro
     carro_da_semana = data_preco(Carros.query.filter_by(nome=nome_carro_da_semana).first())
     img_carro_da_semana = str(carro_da_semana.nome).replace(' ','-')
-    return render_template('home.html',carro_da_semana=carro_da_semana, img_carro_da_semana=img_carro_da_semana)
+    return render_template('home.html',carro_da_semana=carro_da_semana, img_carro_da_semana=img_carro_da_semana,nome_carro_da_semana=nome_carro_da_semana)
+
+
+
+@app.route('/logar',methods=['POST'])
+def processar_login():
+    c = 0
+    data = request.form
+    email_usuario = data.get('email_login')
+    senha_usuario = data.get('senha_login')
+    usuarios = Clientes.query.all()
+
+    for usuario in usuarios:
+        if email_usuario == usuario.email:
+            c += 1
+        if senha_usuario == usuario.senha:
+            c += 1
+    print(c)
+    if c == 3:
+        return redirect(url_for('carros'))
+    else:
+        return redirect(url_for('home'))
+
 
 @app.route('/sobre nós')
 def sobre():
     return render_template('sobre.html')
 
-
+@app.route('/layout')
+def lay():
+    return render_template('layout.html')
 
 @app.route('/processamento',methods=['POST'])
 def processar():
-    c = 0
+    carros_list = []
     data = request.form
-    keys_list = ['marcas','select_registro_inicio', 'select_registro_fim', 'select_preco_inicio', 'select_preco_final', 'select_quilometro_inicio', 'select_quilometro_final','combustivel','estado']
-    for item in data.listvalues():
-        print(f"'{keys_list[c]}':{item}")
-        c += 1
-    return redirect('/carros')
+    carros = Carros.query.all()
+    for carro in carros:
+        marca_carro = str(carro.nome.split()[0])
+        if marca_carro in data.getlist('marcas'):
+            carros_list.append(carros)
+            return url_for(carros)
 
 
 @app.route('/carros')
@@ -73,26 +98,34 @@ def carros():
     precos = list(range(30000,50000,2000)) 
     quilometragem = list(range(1000,30000,3000))
     carros = Carros.query.all()
-    print(request.form[''])
+
 
     for carro in carros:
-        marca_nome_carro = str(carro.nome).replace(' ','-') #tira espaços do no nome do carro
         marca_carro = str(carro.nome.split()[0]) # pega somente a marca do carro
-        carro.registro = carro.registro.strftime('%d/%m/%Y') #corrige formato da data 
-        carro.preco = moedinha(carro.preco) # corrige formatação do valor monetário do carro
-        lst_marcas_carros.append(marca_nome_carro)
         if marca_carro not in lst_marcas:
             lst_marcas.append(marca_carro)
+        if  carro.id <= 10:
+            marca_nome_carro = str(carro.nome).replace(' ','-') #tira espaços do no nome do carro
+            carro.registro = carro.registro.strftime('%d/%m/%Y') #corrige formato da data 
+            carro.preco = moedinha(carro.preco) # corrige formatação do valor monetário do carro
+            lst_marcas_carros.append(marca_nome_carro)
+        else:
+            break
             
-    return render_template('carros.html',
-carros=carros[0:5],lst_marcas=lst_marcas,lst_marcas_carros=lst_marcas_carros, anos=anos,
-precos=precos,quilometragem=quilometragem)
+    return render_template('carros.html',carros=carros[0:10],lst_marcas=lst_marcas,lst_marcas_carros=lst_marcas_carros)
 
-@app.route('/carros/<string:nome_carro>')
-def carro_especifico(nome_carro):
-    return render_template('carro_especifico')
+@app.route('/carros/<string:carro_nome>',methods=['POST','GET'])
+def carro_especifico(carro_nome):
+    carro = Carros.query.filter_by(nome=carro_nome).first()
+    marca_nome_carro = str(carro.nome).replace(' ','-')
+    marca_carro = str(carro.nome).split()[0]
+    carro.preco = moedinha(carro.preco)
+    carro.registro = carro.registro.strftime('%d/%m/%Y')
+    return render_template('carro_especifico.html',carro=carro,marca_nome_carro=marca_nome_carro,marca_carro=marca_carro)
 
-
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
 
 
 
