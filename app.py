fb14d7d82c136608819f467f06c496ab31edb0b8
FabricioNascimentop <119469677+FabricioNascimentop@users.noreply.github.com>
2024-03-06 16:47:50 -0300

    desespero

--- app.py
+++ app.py
@@ -127,7 +127,15 @@ def contatos():
 
 
 
+def data_preco(obj_carro):
+    obj_carro.registro = obj_carro.registro.strftime('%d/%m/%Y')
+    obj_carro.preco = moedinha(obj_carro.preco)
+    return obj_carro
 
+def moedinha(numero):
+    numero_formatado = '{:.2f}'.format(numero)
+    parte_inteira, parte_decimal = numero_formatado.split('.')
+    parte_inteira = '{:,}'.format(int(parte_inteira)).replace(',', '.')
+    valor_formatado = f'R$ {parte_inteira},{parte_decimal}'
+    return valor_formatado
 
-if __name__ == "__main__":
-    app.run(debug=True)
